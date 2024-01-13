d98305478b0cdbc3a40fd110ca63eec60097b086
Kirill Ephanov <kirill.ephanov@gmail.com>
2024-01-13 19:52:20 +0300

    The administrator's website has been made

--- store/store_app/admin.py
+++ store/store_app/admin.py
@@ -2,6 +2,30 @@ from django.contrib import admin
 from .models import *
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_title', 'product_price', 'product_discount', 
+                    'product_total_cost', 'product_seller', 'owner', 
+                    'product_category', 'product_photo')
+    list_display_links = ('id', 'product_title', 'product_price', 'product_discount', 
+                    'product_total_cost', 'product_seller', 'owner', 
+                    'product_category', 'product_photo')
+    search_fields = ('product_title', 'product_price', 'product_discount', 
+                     'product_total_cost')
+    list_filter = ('product_seller', 'product_category')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_title')
+    list_display_links = ('id', 'category_title')
+    search_fields = ('category_title',)
+
+
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'seller_title')
+    list_display_links = ('id', 'seller_title')
+    search_fields = ('seller_title',)
+
+
 class BasketAdmin(admin.ModelAdmin):
     list_display = ('id', 'basket_user')
     list_display_links = ('id', 'basket_user')
@@ -13,5 +37,11 @@ class BasketItemAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'basket_item_basket', 'basket_item_product', 'basket_item_quantity')
 
 
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Seller, SellerAdmin)
 admin.site.register(Basket, BasketAdmin)
 admin.site.register(BasketItem, BasketItemAdmin)
+
+admin.site.site_title = 'Online Store API'
+admin.site.site_header = 'Online Store API'
