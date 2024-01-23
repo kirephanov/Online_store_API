f727a357fa1bd3873bdd8ded0ef799fbfcca4851
Kirill Ephanov <kirill.ephanov@gmail.com>
2024-01-23 16:57:35 +0300

    A WEBPField field has been created in the model

--- store/store_app/models.py
+++ store/store_app/models.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.contrib.auth.models import User
+
+from django.utils.translation import gettext_lazy as _
+ 
+from .fields import WEBPField
 
 
 class Product(models.Model):
@@ -7,7 +13,8 @@ class Product(models.Model):
     product_price = models.IntegerField()
     product_discount = models.IntegerField(default=0, blank=True)
     product_total_cost = models.IntegerField(blank=True)
-    product_photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    #product_photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    product_photo = WEBPField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name=_('Image'))
     product_description = models.TextField(max_length=500)
     product_specifications = models.TextField(max_length=500)
     product_delivery = models.TextField(max_length=200)
